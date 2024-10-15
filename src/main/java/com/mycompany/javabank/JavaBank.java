bc5bba414683a521848b030544124080ddea1963
230500226 <125464717+230500226@users.noreply.github.com>
2024-10-15 20:49:02 +0200

    init login panel

--- src/main/java/com/mycompany/javabank/JavaBank.java
+++ src/main/java/com/mycompany/javabank/JavaBank.java
@@ -8,9 +8,10 @@ package com.mycompany.javabank;
  *
  * @author ldxt460s
  */
+
 public class JavaBank {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        LoginPanel.main();
     }
 }
